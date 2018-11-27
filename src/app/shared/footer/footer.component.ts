0e0e99cd33d12c3be9b83e8b7595cf48e353940d
Joni Agüero <aguerojonathanivan@gmail.com>
2018-11-27 15:53:49 -0300

    update footer css

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: '<div class="footer"></div>',
+  template: '<div class="footer">Footer</div>',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
