8c32936df72d2639f9df2e8075954757912c8464
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-02 20:34:23 -0300

    Gnome Friends

--- src/app/gnomes-components/friends-gnome/friends-gnome.component.ts
+++ src/app/gnomes-components/friends-gnome/friends-gnome.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-friends-gnome',
   templateUrl: './friends-gnome.component.html',
   styleUrls: ['./friends-gnome.component.scss']
 })
-export class FriendsGnomeComponent implements OnInit {
+export class FriendsGnomeComponent {
+
+  @Input() friends: string[];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
