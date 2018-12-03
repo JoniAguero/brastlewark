8c3684032a929197477ad98c7b3a8bab83382b72
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-02 22:27:34 -0300

    Subscription - unsubscribe

--- src/app/gnomes-components/info-gnome/info-gnome.component.ts
+++ src/app/gnomes-components/info-gnome/info-gnome.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/redux/app.reducer';
 import { Gnome } from '../../utils/models/Gnome.model';
 import { Router } from '@angular/router';
+import { Observer, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-info-gnome',
   templateUrl: './info-gnome.component.html',
   styleUrls: ['./info-gnome.component.scss']
 })
-export class InfoGnomeComponent implements OnInit {
+export class InfoGnomeComponent implements OnInit, OnDestroy {
 
   gnomeSelected: Gnome;
+  subscription: Subscription;
 
   constructor(private store: Store<AppState>, private router: Router) { }
   ngOnInit() {
-    this.store.select(state => state.gnomes.gnomeSelected).subscribe(
+    this.subscription = this.store.select(state => state.gnomes.gnomeSelected).subscribe(
       (data: any) => {
         if (data === undefined) {
           this.router.navigate(['/gnomes']);
@@ -27,4 +29,7 @@ export class InfoGnomeComponent implements OnInit {
     );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
