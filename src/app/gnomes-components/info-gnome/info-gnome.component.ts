b5897bb6321ae0900db803c9c201682859a73d58
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-03 08:24:16 -0300

    Layout 1024

--- src/app/gnomes-components/info-gnome/info-gnome.component.ts
+++ src/app/gnomes-components/info-gnome/info-gnome.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/redux/app.reducer';
 import { Gnome } from '../../utils/models/Gnome.model';
-import { Router } from '@angular/router';
-import { Observer, Observable, Subscription } from 'rxjs';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SetGnomeSelected } from 'src/app/redux/actions/gnomes.actions';
 
 @Component({
   selector: 'app-info-gnome',
@@ -13,17 +15,38 @@ import { Observer, Observable, Subscription } from 'rxjs';
 export class InfoGnomeComponent implements OnInit, OnDestroy {
 
   gnomeSelected: Gnome;
+  idSelected: number;
   subscription: Subscription;
 
-  constructor(private store: Store<AppState>, private router: Router) { }
+  constructor(private store: Store<AppState>, private router: Router, private routeActive: ActivatedRoute) { }
+
   ngOnInit() {
+    this.setGnomeSeletected();
+    this.routeActive.params.subscribe(
+      (params: Params) => {
+        if (this.idSelected && parseInt(params.id, 0) !== this.idSelected) {
+          this.store.select(state => state.gnomes.gnomes).pipe(
+            map(gnome =>
+              this.gnomeSelected = gnome.find(function (x) {
+                return x.id === parseInt(params.id, 0);
+              })
+            )
+          ).subscribe().unsubscribe();
+          this.store.dispatch(new SetGnomeSelected(this.gnomeSelected));
+          this.router.navigate([`gnome/${this.gnomeSelected.id}`]);
+        }
+      }
+    );
+  }
+  setGnomeSeletected() {
     this.subscription = this.store.select(state => state.gnomes.gnomeSelected).subscribe(
       (data: any) => {
         if (data === undefined) {
           this.router.navigate(['/gnomes']);
+        } else {
+          this.gnomeSelected = data;
+          this.idSelected = data.id;
         }
-        this.gnomeSelected = data;
-        console.log(this.gnomeSelected);
       },
       err => console.error(err)
     );
