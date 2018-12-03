b93d2707365acd6fb6b7063aedd18bbb2cf81b79
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-02 23:58:21 -0300

    Responsive 768px

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: '<div class="footer">Footer</div>',
+  template: `<div class="footer">
+              Jonathan Agüero - {{year}}
+             </div>`,
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+  year = new Date().getFullYear();
 
   constructor() { }
 
